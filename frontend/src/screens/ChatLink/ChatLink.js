be392a0f83549a99f2f098a90d578c1867adc540
rhhhung <hungyi@uw.edu>
2025-03-10 18:46:58 -0700

    ChatLInk Main

--- frontend/src/screens/ChatLink/ChatLink.js
+++ frontend/src/screens/ChatLink/ChatLink.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './ChatLink.css';
 
+
 const ChatLink = () => {
   return (
     <div className="chatlink-screen">
-      <h1>Chat Link Screen</h1>
+      <div className="chatlink-Title">
+        <h1>Chat Link Screen</h1>
+      </div>
+      <div className="chatlink-SearchFilter">
+
+      </div>
+      <div className="chatlink-Edits">
+
+      </div>
+      <div classaName = "chatlink-links">
+        
+      </div>
     </div>
   );
 };
