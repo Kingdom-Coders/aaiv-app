5b98b9d0478f6dcefa2648af30a59c5e27ccc627
Ken <kenoh17@uw.edu>
2025-03-29 00:52:45 -0700

    started discussion board

--- frontend/src/screens/DiscussionBoard/Discussion/Discussion.js
+++ frontend/src/screens/DiscussionBoard/Discussion/Discussion.js
@@ -4,64 +4,87 @@ import { useNavigate } from "react-router-dom";
 import { logout } from "../../../actions/userActions";
 import "./Discussion.css";
 import { deletePostAction, listPosts } from "../../../actions/postActions";
+import { BsTrash3 } from "react-icons/bs";
+import { Icon } from "@chakra-ui/react";
 
 const Discussion = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const postList = useSelector(state => state.postList);
+  const postList = useSelector((state) => state.postList);
   const { loading, posts, error } = postList;
 
   const postCreate = useSelector((state) => state.postCreate);
   const { success: successCreate } = postCreate;
 
   const postDelete = useSelector((state) => state.postDelete);
-  const { loading: loadingDelete, error: errorDelete, success: successDelete } = postDelete
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = postDelete;
 
   const logoutHandler = () => {
     dispatch(logout());
-  }
+  };
 
   const deleteHandler = (id) => {
-    if(window.confirm("Are you sure?")) {
-        dispatch(deletePostAction(id));
+    if (window.confirm("Are you sure?")) {
+      dispatch(deletePostAction(id));
     }
-  }
+  };
 
   useEffect(() => {
     dispatch(listPosts());
-  }, [dispatch, successCreate, navigate, successDelete])
+  }, [dispatch, successCreate, navigate, successDelete]);
 
   return (
     <div className="container">
       <h2>Posts</h2>
       <div className="buttonsContainer">
-      <div className="signoutButton" onClick={() => {navigate('/createpost')}}>
-        Create Post
-      </div>
-      <div className="signoutButton" onClick={() => { logoutHandler(); navigate('/'); }}>
+        <div
+          className="signoutButton"
+          onClick={() => {
+            navigate("/createpost");
+          }}
+        >
+          Create Post
+        </div>
+        {/* <div className="signoutButton" onClick={() => { logoutHandler(); navigate('/'); }}>
         Sign Out
-      </div>
-      <div className="postsContainer">
-        {posts?.length ? (
+      </div> */}
+        <div className="postsContainer">
+          {posts?.length ? (
             posts.reverse().map((post) => (
-            <div key={post._id} className="post">
-                <h3>{post.title}</h3>
-                <p>{post.body}</p>
-                <p className="date">Created On: {post.createdAt.substring(0, 10)}</p>
+              <div key={post._id} className="post">
+                <div className="content">
+                  <h3>{post.title}</h3>
+                  <p>{post.body}</p>
+                  <p className="date">
+                    Created On: {post.createdAt.substring(0, 10)}
+                  </p>
+                </div>
                 <div
-                className="deleteButton"
-                onClick={() => deleteHandler(post._id)}
-              >
-                X
+                  className="deleteButton"
+                  onClick={() => deleteHandler(post._id)}
+                >
+                  <Icon
+                    size="lg"
+                    color="red.500"
+                    cursor="pointer"
+                    _hover={{ scale: "1.1" }}
+                    transition="0.5s"
+                  >
+                    <BsTrash3 />
+                  </Icon>
+                </div>
               </div>
-            </div>
             ))
-        ) : (
+          ) : (
             <p>No posts found.</p>
-        )}
-       </div>
-    </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
