9c17e23011ce4ac88641009a9afc4ede7ffefafd
eric_cho <eric3cho@gmail.com>
2025-03-31 18:18:06 -0700

    month view

--- frontend/src/screens/Calendar/Calendar.js
+++ frontend/src/screens/Calendar/Calendar.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calendar.css';
+import { DayPilot, DayPilotMonth } from "@daypilot/daypilot-lite-react";
 
 const Calendar = () => {
+  const [startDate] = useState(DayPilot.Date.today());
+  const [monthView] = useState(null);
+
   return (
     <div className="calendar-screen">
-      <h1>Calendar Screen</h1>
+      <div className="calendar-title">
+        <h1>Calendar Screen</h1>
+      </div>
+      <div className="calendar-month-view">
+        <h1>Month View Screen</h1>
+          <DayPilotMonth
+            startDate={startDate}
+            headerHeight={60}
+            cellHeaderHeight={25}
+            width="100%"
+            controlRef={monthView}  
+          />
+      </div>
+      <div className="calendar-new-event">
+        <h1>New Event Screen</h1>
+      </div>
+      <div className="calendar-week-view">
+        <h1>Week View Screen</h1>
+      </div>
     </div>
   );
 };
