8ffadfee55cb03c3f4b6edf374c4a9031e80b69b
Ken Oh <kenoh17@uw.edu>
2025-04-03 18:12:30 -0700

    updated cards to accordion

--- frontend/src/screens/DiscussionBoard/Discussion/Discussion.js
+++ frontend/src/screens/DiscussionBoard/Discussion/Discussion.js
@@ -7,8 +7,7 @@ import { deletePostAction, listPosts } from "../../../actions/postActions";
 import { BsTrash3 } from "react-icons/bs";
 import { GrMore } from "react-icons/gr";
 import { LuMessageSquareText } from "react-icons/lu";
-import { Button, Menu, Portal } from "@chakra-ui/react";
-import { Avatar, Card } from "@chakra-ui/react";
+import { Button, Menu, Portal, Card, Accordion } from "@chakra-ui/react";
 
 const Discussion = () => {
   const navigate = useNavigate();
@@ -56,78 +55,60 @@ const Discussion = () => {
         {/* <div className="signoutButton" onClick={() => { logoutHandler(); navigate('/'); }}>
         Sign Out
       </div> */}
-        <div className="postsContainer">
+        <Accordion.Root collapsible className="postsContainer">
           {posts?.length ? (
-            posts.reverse().map((post) => (
-              /* <div key={post._id} className="post">
-                <div className="content">
-                  <h3>{post.title}</h3>
-                  <p>{post.body}</p>
-                  <p className="date">
-                    Created On: {post.createdAt.substring(0, 10)}
-                  </p>
-                </div>
-
-                <div className="buttons">
-                  <div className="button reply">
-                    <LuMessageSquareText />
-                    <p>Reply</p>
+            posts.reverse().map((post, index) => (
+              <Accordion.Item key={index} value={index} className="post">
+                <Accordion.ItemTrigger className="title-container">
+                  <h1 className="title">
+                    {post.title}{" "}
+                    <span className="date">
+                      {post.createdAt.substring(0, 10)}
+                    </span>{" "}
+                  </h1>
+                  <Accordion.ItemIndicator />
+                </Accordion.ItemTrigger>
+                <Accordion.ItemContent>
+                  <Accordion.ItemBody color="black" className="content">
+                    <p>{post.body}</p>
+                  </Accordion.ItemBody>
+                  <div className="buttons">
+                    <Button variant="ghost" size="md" className="button">
+                      <LuMessageSquareText />
+                      Reply
+                    </Button>
+                    <Menu.Root>
+                      <Menu.Trigger asChild>
+                        <Button variant="ghost" size="md" className="button">
+                          <GrMore />
+                        </Button>
+                      </Menu.Trigger>
+                      <Portal>
+                        <Menu.Positioner>
+                          <Menu.Content>
+                            <Menu.Item value="edit">Edit</Menu.Item>
+                            <Menu.Item value="report">Report</Menu.Item>
+                            <Menu.Item
+                              value="delete"
+                              color="fg.error"
+                              _hover={{ bg: "bg.error", color: "fg.error" }}
+                              onClick={() => deleteHandler(post._id)}
+                            >
+                              Delete...
+                            </Menu.Item>
+                          </Menu.Content>
+                        </Menu.Positioner>
+                      </Portal>
+                    </Menu.Root>
                   </div>
-                  <div
-                    className="button delete"
-                    onClick={() => deleteHandler(post._id)}
-                  >
-                  </div>
-                  
-                </div>
-              </div> */
-
-              <Card.Root className="post">
-                <Card.Body gap="2" className="content">
-                  {/* <Avatar.Root size="lg" shape="rounded">
-                    <Avatar.Image src="https://picsum.photos/200/300" />
-                    <Avatar.Fallback name="Nue Camp" />
-                  </Avatar.Root> */}
-                  <Card.Title mt="2">{post.title}</Card.Title>
-                  <Card.Description>{post.body}</Card.Description>
-                </Card.Body>
-                <Card.Footer justifyContent="flex-end" className="buttons">
-                  <Button variant="ghost" size="sm" className="button">
-                    <LuMessageSquareText />
-                    Reply
-                  </Button>
-                  <Menu.Root>
-                    <Menu.Trigger asChild>
-                      <Button variant="ghost" size="sm" className="button">
-                        <GrMore />
-                      </Button>
-                    </Menu.Trigger>
-                    <Portal>
-                      <Menu.Positioner>
-                        <Menu.Content>
-                          <Menu.Item value="edit">Edit</Menu.Item>
-                          <Menu.Item value="report">Report</Menu.Item>
-                          <Menu.Item
-                            value="delete"
-                            color="fg.error"
-                            _hover={{ bg: "bg.error", color: "fg.error" }}
-                            onClick={() => deleteHandler(post._id)}
-                          >
-                            Delete...
-                          </Menu.Item>
-                        </Menu.Content>
-                      </Menu.Positioner>
-                    </Portal>
-                  </Menu.Root>
-                </Card.Footer>
-              </Card.Root>
+                </Accordion.ItemContent>
+              </Accordion.Item>
             ))
           ) : (
             <p>No posts found.</p>
           )}
-        </div>
+        </Accordion.Root>
       </div>
-      {/* <div className="filler"></div> */}
     </div>
   );
 };
