7a5100612395db210f97eb0041414c96aec12fe5
Beverley Kirkwood <beverleykirkwood@Beverleys-MacBook-Air.local>
2025-04-14 14:12:59 -0700

    announcement

--- frontend/src/screens/Admin/Admin.js
+++ frontend/src/screens/Admin/Admin.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './Admin.css';
+import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
+import { Input } from "@chakra-ui/react"
+import { Textarea } from "@chakra-ui/react"
 
 const Admin = () => {
   return (
     <div className="admin-screen">
+<<<<<<< Updated upstream
       <h1>Admin Screen</h1>
     </div>
+=======
+      <h1 className="titleText">Admin Screen</h1>
+      <UserList />
+      <div className="announcement-post">
+        <div className="announcement-title">
+          <h1>Announcement</h1>
+        </div>
+        <h2> Title </h2>
+        <Input style={{
+          marginBottom: '5%'
+        }}/>
+        <h2> Body </h2>
+        <div className="text-area">
+          <Textarea  style={{
+          marginBottom: '5%'
+          }}/>
+        </div>
+        <div className="button">
+          <Button> Post </Button>
+        </div>
+        </div>
+
+        </div>
+>>>>>>> Stashed changes
   );
 };
 
