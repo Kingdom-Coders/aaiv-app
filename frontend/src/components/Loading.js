3bf1dde3928cc1237fc1e9b52f959779bc562502
ellie <dabinellie@gmail.com>
2025-05-28 22:57:02 -0700

    lit spinner i hope this meets criteria

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
 
-function Loading({ size = 100 }) {
+//Set the default message and size
+// Added margin bottom as space
+function Loading({ size = 100, loadingMessage="Loading ...", marginBot="30px" }) {
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
-        height: "100%",
+        height: "100%"
       }}
     >
       <Spinner
         style={{
           width: size,
           height: size,
+          marginBottom: marginBot,
         }}
+        //Can change the style and color here
         animation="border"
+        variant="secondary" 
       />
+      {loadingMessage}
     </div>
   );
 }
