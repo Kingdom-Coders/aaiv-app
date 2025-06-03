cc27daf2f960406934c3f4dc0763b089b78c918d
DavyHyun <45442281+DavyHyun@users.noreply.github.com>
2025-06-03 02:35:24 -0700

    coinfig

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'aaivapp',
+  appId: 'com.helloship.app',
+  appName: 'Helloship',
   webDir: 'frontend/build'
 };
 
